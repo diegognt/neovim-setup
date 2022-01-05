0422665e3c8823e4f885e7601bb5700b8d630209
Diego Navarro <diego.nava07@gmail.com>
2022-01-04 22:25:46 -0500

    [LSP] Initial LSP set up.
    This PR includes,
      - LSP installer plugin configuration
      - LSP integration w/ completion.
      - LSP settings for Lua.
      - LSP settings for JSON.

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
-require 'diegognt.options' 
+require 'diegognt.options'
 require 'diegognt.keymaps'
 require 'diegognt.plugins'
-require 'diegognt.colorscheme' 
+require 'diegognt.colorscheme'
 require 'diegognt.completion'
+require 'diegognt.lsp'
