19781aa22177ca5a4032537252c8ccdc5cd3a0bf
Diego Navarro <diego.nava07@gmail.com>
2022-01-10 21:02:50 -0500

    [Autopairs] Initial set up.

--- lua/diegognt/plugins.lua
+++ lua/diegognt/plugins.lua
@@ -75,6 +75,7 @@ return packer.startup(function(use)
   }
   use 'folke/twilight.nvim'
   use 'p00f/nvim-ts-rainbow'
+  use 'windwp/nvim-autopairs'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
