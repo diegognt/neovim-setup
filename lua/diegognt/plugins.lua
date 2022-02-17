9a6b23719baacfe092fa5d5816af6d54177ae666
Diego Navarro <diego.nava07@gmail.com>
2022-02-16 22:45:35 -0500

    [Statusline] Initial set up.
    This PR includes,
      - Use `nvim-lualine/lualine.nvim` to customize the statusline.
      - nvim-lualine initial set up.

--- lua/diegognt/plugins.lua
+++ lua/diegognt/plugins.lua
@@ -90,6 +90,9 @@ return packer.startup(function(use)
   -- Git stuff
   use 'lewis6991/gitsigns.nvim'
 
+  -- Status line.
+  use 'nvim-lualine/lualine.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
