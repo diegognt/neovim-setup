ff231ebbd5d13a4322524433038e0c9ba2aca166
Diego Navarro <diego.nava07@gmail.com>
2022-02-21 13:21:12 -0500

    [Autocommands] Initial set up
    This PR incliudes:
      - Seting up several useful autocommands.

---
+++ lua/diegognt/autocommands.lua
@@ -0,0 +1,27 @@
+vim.cmd [[
+  augroup _general_settings
+    autocmd!
+    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200}) 
+    autocmd BufWinEnter * :set formatoptions-=cro
+    autocmd FileType qf set nobuflisted
+  augroup END
+  augroup _git
+    autocmd!
+    autocmd FileType gitcommit setlocal wrap
+    autocmd FileType gitcommit setlocal spell
+  augroup END
+  augroup _markdown
+    autocmd!
+    autocmd FileType markdown setlocal wrap
+    autocmd FileType markdown setlocal spell
+  augroup END
+  augroup _auto_resize
+    autocmd!
+    autocmd VimResized * tabdo wincmd = 
+  augroup END
+  augroup _alpha
+    autocmd!
+    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+  augroup END
+]]
