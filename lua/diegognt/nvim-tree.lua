fa3b642c702af37be0386e6767c64211b91fe74f
Diego Navarro <diego.nava07@gmail.com>
2022-05-19 11:17:35 -0500

    [File Explorer] nvim-tree configuration updates.
    This PR includes:
     - Deprecated properties for the config has been removed.

--- lua/diegognt/nvim-tree.lua
+++ lua/diegognt/nvim-tree.lua
@@ -39,12 +39,7 @@ nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
   open_on_setup = false,
-  ignore_ft_on_setup = {
-    'startify',
-    'dashboard',
-    'alpha',
-  },
-  auto_close = true,
+  ignore_ft_on_setup = {'startify', 'dashboard', 'alpha'},
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
@@ -87,16 +82,5 @@ nvim_tree.setup {
     },
     number = false,
     relativenumber = false,
-  },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ':t',
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
-  },
+  }
 }
