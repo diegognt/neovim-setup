86848a15a77a553c1f286d12883eab6eb968a675
Diego Navarro <diego.nava07@gmail.com>
2022-07-10 21:20:29 -0500

    [Colorscheme] Adding 'Kanagawa' colorscheme.
    This PR includes:
      - New colorscheme, Kanagawa
      - Options per colorscheme

--- lua/diegognt/colorscheme.lua
+++ lua/diegognt/colorscheme.lua
@@ -1,11 +1,19 @@
-local colorscheme = "tokyonight"
+local colorscheme = 'tokyonight'
 
 -- Tokyo Night set up
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+if colorscheme == 'tokyonight' then
+	vim.g.tokyonight_italic_functions = true
+	vim.g.tokyonight_sidebars = { 'qf', 'vista_kind', 'terminal', 'packer' }
+end
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if colorscheme == 'kanagawa' then
+	require('kanagawa').setup({
+		functionStyle = { italic = true },
+	})
+end
+
+local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
+	vim.notify('colorscheme ' .. colorscheme .. ' not found!')
+	return
 end
