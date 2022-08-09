fca2e400cbfea53c360c4f5bfe31c639764d78eb
Diego Navarro <diego.nava07@gmail.com>
2022-08-09 07:37:53 -0500

    [Annotation] Initial set up.
    This PR includes:
      - Use of the `danymat/neogen` plugin to do annotations.
      - Initial keybindings for annotations.
      - Initial set up for the `danymat/neogen` plugin.

--- lua/diegognt/plugins.lua
+++ lua/diegognt/plugins.lua
@@ -59,6 +59,7 @@ return packer.startup(function(use)
   use 'goolord/alpha-nvim' -- A simple greeter screen with style
   use 'lewis6991/impatient.nvim'
   use 'folke/todo-comments.nvim' -- Fancy TODO manager
+  use 'danymat/neogen' -- Doc annotation
 
   -- File Explorer
   use 'kyazdani42/nvim-tree.lua' -- Nvim file explorer
