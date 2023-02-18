57c908a6b83a810b6b01e5f4e44fcde1a7fc9b61
Diego Navarro <code@diegognt.dev>
2023-02-18 08:08:28 -0500

    Adding Lazyvim as a plugin manager.
    This Pr includes:
      - Deprecation of Packer as plugin manager
      - Configuration on Lazyvim as plugin manager

--- lua/diegognt/plugins.lua
+++ lua/diegognt/plugins.lua
@@ -2,128 +2,118 @@
 
 local fn = vim.fn
 
--- Automatically install packer
--- 'fn.stdpath' is the following path '.local/share/nvim/'
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
+-- Autoinstall LazyVim if not installed
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     'git',
     'clone',
-    '--depth',
-    '1',
-    'https://github.com/wbthomason/packer.nvim',
-    install_path,
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
   })
-  print('Installing packer close and reopen Neovim...')
-  vim.cmd([[packadd packer.nvim]])
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+vim.opt.rtp:prepend(lazypath)
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, 'packer')
+local status_ok, lazy = pcall(require, 'lazy')
 if not status_ok then
+  vim.notify('LazyVim is not installed')
   return
 end
 
--- Have packer use a popup window
-packer.init({
-  display = {
-    open_fn = function()
-      return require('packer.util').float({ border = 'rounded' })
-    end,
-  },
-})
-
 -- Install your plugins here
-return packer.startup(function(use)
+return lazy.setup({
   -- My plugins here
-  use('wbthomason/packer.nvim') -- Have packer manage itself
-  use('nvim-lua/popup.nvim') -- An implementation of the Popup API from vim in Neovim
-  use('nvim-lua/plenary.nvim') -- Useful lua functions used ny lots of plugins
-  use('kyazdani42/nvim-web-devicons') -- Nice icons
-  use('folke/twilight.nvim') -- Focus on code stuff
-  use('windwp/nvim-autopairs') -- Autopairs plugin with treesitter and cmp
-  use('numToStr/Comment.nvim') -- Comments make it easy
-  use('akinsho/bufferline.nvim') -- Bufferline plugin
-  use('moll/vim-bbye') -- Plugin to avoid close nvim when close a buffer
-  use('akinsho/toggleterm.nvim') --Nice Terminal for nvim
-  use('lukas-reineke/indent-blankline.nvim') -- Nice indentation
-  use('ahmedkhalf/project.nvim') -- Cool projects/workspaces manager
-  use('goolord/alpha-nvim') -- A simple greeter screen with style
-  use('lewis6991/impatient.nvim')
-  use('folke/todo-comments.nvim') -- Fancy TODO manager
-  use('danymat/neogen') -- Doc annotation
+  'nvim-lua/popup.nvim', -- An implementation of the Popup API from vim in Neovim
+  'nvim-lua/plenary.nvim', -- Useful lua functions used ny lots of plugins
+  'kyazdani42/nvim-web-devicons', -- Nice icons
+  'folke/twilight.nvim', -- Focus on code stuff
+  'windwp/nvim-autopairs', -- Autopairs plugin with treesitter and cmp
+  'numToStr/Comment.nvim', -- Comments make it easy
+  'akinsho/bufferline.nvim', -- Bufferline plugin
+  'moll/vim-bbye', -- Plugin to avoid close nvim when close a buffer
+  'akinsho/toggleterm.nvim', --Nice Terminal for nvim
+  'lukas-reineke/indent-blankline.nvim', -- Nice indentation
+  'ahmedkhalf/project.nvim', -- Cool projects/workspaces manager
+  'goolord/alpha-nvim', -- A simple greeter screen with style
+  'lewis6991/impatient.nvim',
+  'folke/todo-comments.nvim', -- Fancy TODO manager
+  'danymat/neogen', -- Doc annotation
 
   -- File Explorer
-  use('kyazdani42/nvim-tree.lua') -- Nvim file explorer
+  'kyazdani42/nvim-tree.lua', -- Nvim file explorer
 
   -- Tselescope
-  use('nvim-telescope/telescope.nvim')
-  use('nvim-telescope/telescope-project.nvim')
+  'nvim-telescope/telescope.nvim',
+  'nvim-telescope/telescope-project.nvim',
 
   -- Colorschemes
-  use('folke/tokyonight.nvim')
-  use('rebelot/kanagawa.nvim')
-  use('EdenEast/nightfox.nvim')
+  'folke/tokyonight.nvim',
+  'rebelot/kanagawa.nvim',
+  'EdenEast/nightfox.nvim',
 
   --Completion plugins
-  use('hrsh7th/nvim-cmp') -- The completion plugin
-  use('hrsh7th/cmp-buffer') -- The buffer completions plugin extension
-  use('hrsh7th/cmp-path') -- The path completions plugin extension
-  use('hrsh7th/cmp-cmdline') -- The cmdline completions plugin extension
-  use('saadparwaiz1/cmp_luasnip') -- The LuaSnip completions plugin extension
-  use('hrsh7th/cmp-nvim-lsp') -- The LSP completion plugin extension
-  use('hrsh7th/cmp-nvim-lua') -- The Lua language completion plugin extension
-
+  {
+    'hrsh7th/nvim-cmp', -- The completion plugin
+    dependencies = {
+      'hrsh7th/cmp-buffer', -- The buffer completions plugin extension
+      'hrsh7th/cmp-path', -- The path completions plugin extension
+      'hrsh7th/cmp-cmdline', -- The cmdline completions plugin extension
+      'saadparwaiz1/cmp_luasnip', -- The LuaSnip completions plugin extension
+      'hrsh7th/cmp-nvim-lsp', -- The LSP completion plugin extension
+      'hrsh7th/cmp-nvim-lua', -- The Lua language completion plugin extension
+    },
+  },
   -- Snippets
-  use('L3MON4D3/LuaSnip') -- A snippet engine
-  use('rafamadriz/friendly-snippets') -- A bunch of snippets to use
+  'L3MON4D3/LuaSnip', -- A snippet engine
+  'rafamadriz/friendly-snippets', -- A bunch of snippets to use
 
   -- Language Server Protocol - LSP
-  use('neovim/nvim-lspconfig') -- Enables LSP
-  use 'williamboman/mason.nvim' -- Manager
-  use 'williamboman/mason-lspconfig.nvim' -- LSP adapter for Mason
-  use('jose-elias-alvarez/null-ls.nvim') -- null-ls
-  use('folke/trouble.nvim') -- Elegant diagnostic tools for LSP
-  use('RRethy/vim-illuminate') -- Highlighting words occurance
-
+  {
+    'neovim/nvim-lspconfig', -- Enables LSP
+    dependencies = {
+      'williamboman/mason.nvim', -- Manager
+      'williamboman/mason-lspconfig.nvim', -- LSP adapter for Mason
+      'jose-elias-alvarez/null-ls.nvim', -- null-ls
+      'jayp0521/mason-nvim-dap.nvim', -- Install the required tools for adapters
+      'folke/trouble.nvim', -- Elegant diagnostic tools for LSP
+      'RRethy/vim-illuminate', -- Highlighting words occurance
+    },
+  },
   -- TreeSitter
-  use({
+  {
     'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-  })
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
-  use('p00f/nvim-ts-rainbow') -- Better parentheses rainbow using treesitter
-  use('JoosepAlviste/nvim-ts-context-commentstring')
-  use('nvim-treesitter/nvim-tree-docs')
-  use('windwp/nvim-ts-autotag')
-
+    build = function()
+      pcall(require('nvim-treesitter.install').update({ with_sync = true }))
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'p00f/nvim-ts-rainbow', -- Better parentheses rainbow using treesitter
+      'JoosepAlviste/nvim-ts-context-commentstring',
+      'nvim-treesitter/nvim-tree-docs',
+      'windwp/nvim-ts-autotag',
+    },
+  },
   -- Git stuff
-  use('lewis6991/gitsigns.nvim')
+  'lewis6991/gitsigns.nvim',
 
   -- Status line.
-  use('nvim-lualine/lualine.nvim')
+  'nvim-lualine/lualine.nvim',
 
-  use('folke/which-key.nvim') -- Keymap helper
+  'folke/which-key.nvim', -- Keymap helper
 
   -- Debugger Adapter Protocol
-  use('mfussenegger/nvim-dap')
-  use('rcarriga/nvim-dap-ui')
-  use('theHamsta/nvim-dap-virtual-text')
-  use('leoluz/nvim-dap-go') -- GO adapter
-  use('mfussenegger/nvim-dap-python') -- Python adapter
-  use('mxsdev/nvim-dap-vscode-js') -- JS/TS adapter
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require('packer').sync()
-  end
-end)
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'theHamsta/nvim-dap-virtual-text',
+      'leoluz/nvim-dap-go', -- GO adapter
+      'mfussenegger/nvim-dap-python', -- Python adapter
+      'mxsdev/nvim-dap-vscode-js', -- JS/TS adapter
+    },
+  },
+})
