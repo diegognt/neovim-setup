ef7270d189e1349b08df11e6e59af290da13fa7c
Diego Navarro <code@diegognt.dev>
2023-02-18 08:28:56 -0500

    [Autocommands] Adding new autocommands for productivity.

--- lua/diegognt/autocommands.lua
+++ lua/diegognt/autocommands.lua
@@ -32,10 +32,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- close some filetypes with <q>
 vim.api.nvim_create_autocmd('FileType', {
   group = GeneralSettingsGroup,
-  pattern = { 'qf', 'help', 'man', 'lspinfo', 'lsp-installer', 'null-ls-info' },
-  command = 'nnoremap <silent> <buffer> q :close<CR>',
+  pattern = {
+    'qf',
+    'help',
+    'man',
+    'notify',
+    'lspinfo',
+    'spectre_panel',
+    'startuptime',
+    'tsplayground',
+    'PlenaryTestPopup',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+  end,
 })
 
 vim.api.nvim_create_autocmd('FileType', {
@@ -50,12 +64,17 @@ vim.api.nvim_create_autocmd({ 'BufWinEnter', 'BufRead', 'BufNewFile' }, {
   command = 'setlocal formatoptions-=c formatoptions-=r formatoptions-=o',
 })
 
+-- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd('FileType', {
   group = SpelledGroup,
   pattern = { 'gitcommit', 'markdown' },
-  command = 'setlocal wrap spell',
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
 })
 
+-- Astro filetype
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufEnter' }, {
   group = AstroGroup,
   pattern = '*.astro',
@@ -66,4 +85,16 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufEnter' }, {
   group = CodeAnalysisGroup,
   pattern = { '.prettierrc', '.stylelintrc' },
   command = 'set filetype=json',
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = GeneralSettingsGroup,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
 })
