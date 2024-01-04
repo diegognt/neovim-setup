39bda4d1faa660f75449c7a877775124a1a5ee3d
Diego Navarro <code@diegognt.dev>
2024-01-04 17:48:59 -0500

    Set up Lazy to import plugins as specs.

--- init.lua
+++ init.lua
@@ -1,12 +1,20 @@
-require('diegognt.options')
-require('diegognt.keymaps')
-require('diegognt.plugins')
-require('diegognt.colorscheme')
-require('diegognt.lsp')
-require('diegognt.gitsigns')
-require('diegognt.telescope')
-require('diegognt.toggleterm')
-require('diegognt.lualine')
-require('diegognt.impatient')
-require('diegognt.autocommands')
-require('diegognt.illuminate')
+require "diegognt.launch"
+require "diegognt.options"
+require "diegognt.keymaps"
+-- Specs goes here
+
+
+-- Specs ends here
+require "diegognt.lazy"
+-- require('diegognt.options')
+-- require('diegognt.keymaps')
+-- require('diegognt.plugins')
+-- require('diegognt.colorscheme')
+-- require('diegognt.lsp')
+-- require('diegognt.gitsigns')
+-- require('diegognt.telescope')
+-- require('diegognt.toggleterm')
+-- require('diegognt.lualine')
+-- require('diegognt.impatient')
+-- require('diegognt.autocommands')
+-- require('diegognt.illuminate')
