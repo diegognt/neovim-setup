0d2a6f9db7565214f5dbc8a3992f22196efa6c8c
Diego Navarro <code@diegognt.dev>
2024-01-04 18:15:53 -0500

    Adds colorscheme spec

--- lua/diegognt/colorscheme.lua
+++ lua/diegognt/colorscheme.lua
@@ -1,29 +1,53 @@
-local colorscheme = 'nordfox'
+local colorscheme = 'nightfox'
 
 -- Tokyo Night set up
-if colorscheme == 'tokyonight' then
-  vim.g.tokyonight_italic_functions = true
-  vim.g.tokyonight_sidebars = { 'qf', 'vista_kind', 'terminal', 'packer' }
-end
+-- if colorscheme == 'tokyonight' then
+  -- require('tokyonight').setup({
+    -- styles = {
+      -- Style to be applied to different syntax groups
+      -- comments = { italic = true },
+       -- keywords = { italic = false },
+    -- },
+  -- })
+-- end
 
 -- TODO: Add each setup when the colorscheme is in used.
-require('kanagawa').setup({
-  functionStyle = { italic = true },
-})
+-- require('kanagawa').setup({
+  -- transparent = true, -- do not set background color
+  -- functionStyle = {},
+  -- commentStyle = { italic = true },
+  -- keywordStyle = { italic = true },
+  -- statementStyle = { bold = true },
+  -- typeStyle = { bold = true, italic = true },
+-- })
 
-require('nightfox').setup({
-  options = {
-    styles = {
-      functions = 'italic',
-      comments = 'italic',
-      keywords = 'bold',
-      types = 'italic,bold',
-    },
+local Spec = {
+  "EdenEast/nightfox.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  opts = {
+    options = {
+      transparent = true,
+      styles = {
+        comments = 'italic', -- Value is any valid attr-list value `:help attr-list`
+        conditionals = 'NONE',
+        constants = 'NONE',
+        functions = 'NONE',
+        keywords = 'italic',
+        numbers = 'NONE',
+        operators = 'NONE',
+        strings = 'NONE',
+        types = 'italic',
+        variables = 'NONE',
+      },
+    }
   },
-})
+}
 
-local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
-if not status_ok then
-  vim.notify('colorscheme ' .. colorscheme .. ' not found!')
-  return
+function Spec.config(_, opts)
+  require "nightfox".setup(opts)
+
+  vim.cmd.colorscheme "duskfox"
 end
+
+return Spec
