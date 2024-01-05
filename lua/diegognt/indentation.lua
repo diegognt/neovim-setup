a52b3a4a67325d7eeeab085aad3848c8b5133106
Diego Navarro <code@diegognt.dev>
2024-01-05 12:03:43 -0500

    Adds indentation spec

--- lua/diegognt/indentation.lua
+++ lua/diegognt/indentation.lua
@@ -1,4 +1,17 @@
-return {
-  draw = { delay = 80 },
-  symbol = '┊',
+local Spec = {
+  "echasnovski/mini.indentscope",
+  dependencies = {
+    "echasnovski/mini.nvim",
+  },
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    draw = { delay = 80 },
+    symbol = "┊",
+  },
 }
+
+function Spec.configfunction(_, opts)
+  require("mini.indentscope").setup(opts)
+end
+
+return Spec
