5094e655fc02a8be16d93dc4e7892f5bc2750616
Diego Navarro <code@diegognt.dev>
2024-01-05 09:04:29 -0500

    Adds status line spec using Lualine

--- lua/diegognt/lualine.lua
+++ lua/diegognt/lualine.lua
@@ -1,35 +1,12 @@
-local hide_in_width = function()
-  return vim.fn.winwidth(0) > 80
-end
-
-local diagnostics = {
-  'diagnostics',
-  sources = { 'nvim_diagnostic' },
-  sections = { 'error', 'warn' },
-  symbols = { error = ' ', warn = ' ' },
-  colored = false,
-  update_in_insert = false,
-  always_visible = true,
-}
-
+local icons = require "diegognt.icons"
 local diff = {
   'diff',
   colored = false,
-  symbols = { added = ' ', modified = ' ', removed = ' ' }, -- changes diff symbols
-  cond = hide_in_width,
-}
-
-local mode = {
-  'mode',
-  fmt = function(str)
-    return '-- ' .. str .. ' --'
-  end,
-}
-
-local filetype = {
-  'filetype',
-  icons_enabled = false,
-  icon = nil,
+  symbols = { 
+    added = icons.git.LineAdded,
+    modified = icons.git.LineModified,
+    removed = icons.git.LineRemoved
+  }, -- Changes the symbols used by the diff.
 }
 
 local branch = {
@@ -38,50 +15,27 @@ local branch = {
   icon = '',
 }
 
-local location = {
-  'location',
-  padding = 0,
+local Spec = {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = { 'alpha', 'dashboard', 'NvimTree', 'Outline' },
+      ignore_focus = { "NvimTree" },
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { branch },
+      lualine_c = { diff },
+      lualine_x = { "diagnostics" },
+      lualine_y = { "filetype" },
+      lualine_z = { "progress" },
+    },
+    extensions = { "quickfix", "man", "fugitive" },
+  },
+  config = true
 }
 
--- cool function for progress
-local progress = function()
-  local current_line = vim.fn.line('.')
-  local total_lines = vim.fn.line('$')
-  local chars = { '__', '▁▁', '▂▂', '▃▃', '▄▄', '▅▅', '▆▆', '▇▇', '██' }
-  local line_ratio = current_line / total_lines
-  local index = math.ceil(line_ratio * #chars)
-  return chars[index]
-end
+return Spec
 
-local spaces = function()
-  return 'spaces: ' .. vim.api.nvim_buf_get_option(0, 'shiftwidth')
-end
-
-return {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
-    disabled_filetypes = { 'alpha', 'dashboard', 'NvimTree', 'Outline' },
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = { branch, diagnostics },
-    lualine_b = { mode },
-    lualine_c = {},
-    lualine_x = { diff, spaces, 'encoding', filetype },
-    lualine_y = { location },
-    lualine_z = { progress },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
-  extensions = {},
-}
