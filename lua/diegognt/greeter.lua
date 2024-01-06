a8cf92a0a93f59d4f1e5e120e12094b80b6785ec
Diego Navarro <code@diegognt.dev>
2024-01-06 10:56:15 -0500

    Adds the Greeter spec

--- lua/diegognt/greeter.lua
+++ lua/diegognt/greeter.lua
@@ -1,39 +1,45 @@
-local status_ok, alpha = pcall(require, 'alpha')
-if not status_ok then
-  vim.notify('The `goolord/alpha-nvim` plugin was not found.')
-  return
-end
+local Spec = {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  config = function()
+    local alpha = require "alpha"
+    local dashboard = require "alpha.themes.dashboard"
 
-local M = {}
+    dashboard.section.header.val = {
+      [[                               __                ]],
+      [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+      [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+      [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+      [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+      [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+    }
 
-M.dashboard = require('alpha.themes.dashboard')
+    dashboard.section.buttons.val = {
+      dashboard.button("f", "  Find file", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button(
+        "r",
+        "  Recently used files",
+        ":Telescope oldfiles <CR>"
+      ),
+      dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button(
+        "c",
+        "  Configuration",
+        ":e ~/.config/nvim/init.lua <CR>"
+      ),
+      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    }
+
+    dashboard.section.header.opts.hl = "Include"
+    dashboard.section.buttons.opts.hl = "Keyword"
+
+    dashboard.opts.opts.noautocmd = true
+
+    alpha.setup(dashboard.opts)
+  end,
+}
+
 -- TODO: Find a better ASCII art.
-M.dashboard.section.header.val = {
-  [[                               __                ]],
-  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-}
 
-M.dashboard.section.buttons.val = {
-  M.dashboard.button(
-    'f',
-    '  Find file',
-    '<cmd>lua require("telescope.builtin").git_files(require("telescope.themes").get_dropdown({previewer = false}))<CR>'
-  ),
-  M.dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
-  M.dashboard.button('p', '  Find project', ':Telescope projects <CR>'),
-  M.dashboard.button('r', '  Recently used files', ':Telescope oldfiles <CR>'),
-  M.dashboard.button('t', '  Find text', ':Telescope live_grep <CR>'),
-  M.dashboard.button('c', '  Configuration', ':e ~/.config/nvim/init.lua <CR>'),
-  M.dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
-}
-
-M.dashboard.section.header.opts.hl = 'Include'
-M.dashboard.section.buttons.opts.hl = 'Keyword'
-
-M.dashboard.opts.opts.noautocmd = true
-
-return M
+return Spec
