6e75fe89c0e143143f455706106c45f128b787fc
Diego Navarro <code@diegognt.dev>
2024-01-07 07:48:32 -0500

    Adds LSP diagnostic spec

---
+++ lua/diegognt/lsp/diagnostic.lua
@@ -0,0 +1,36 @@
+local icons = require "diegognt.icons"
+local Spec = {
+  "folke/trouble.nvim",
+  cmd = { 'Trouble' },
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    auto_close = true, -- automatically close the list when you have no diagnostics
+    auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+    signs = {
+      error = icons.diagnostics.BoldError,
+      warning = icons.diagnostics.BoldWarning,
+      hint = icons.diagnostics.BoldHint,
+      information = icons.diagnostics.BoldInformation,
+      other = icons.diagnostics.BoldQuestion,
+    },
+  },
+}
+
+function Spec.config(_, opts)
+  require("trouble").setup(opts)
+
+  local whichkey = require "which-key"
+
+  whichkey.register({
+    ["<leader>xd"] = { "<cmd>Trouble document_diagnostics<CR>", "Document" },
+    ["<leader>xw"] = { "<cmd>Trouble workspace_diagnostics<CR>", "Workspace" },
+    ["<leader>xl"] = { "<cmd>Trouble loclist<CR>", "Loclist" },
+    ["<leader>xt"] = { "<cmd>TodoTrouble<CR>", "TODOs" },
+    ["<leader>xq"] = { "<cmd>Trouble quickfix<CR>", "Quickfix" },
+  })
+end
+
+return Spec
