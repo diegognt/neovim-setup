4a3d2f15c918cf6b880685f6751ad3c2985970d4
Diego Navarro <code@diegognt.dev>
2024-01-14 20:16:20 -0500

    Enhances the LSP spec
    - Uses a autocommand to attach the LSP keymaps to a buffer
    - Removes unnecessary dependencies

--- lua/diegognt/lsp/init.lua
+++ lua/diegognt/lsp/init.lua
@@ -1,30 +1,14 @@
+local icons = require "diegognt.icons"
 local Spec = {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    { -- Elegant diagnostic tools for lsp_keymaps
-      "folke/trouble.nvim",
-      cmd = { "Trouble" },
-      opts = { use_diagnostic_signs = true },
-    },
     {
       "b0o/schemastore.nvim",
       lazy = true,
     },
   },
 }
-
-local function lsp_keymaps(client, bufnr)
-  local opts = { noremap = true, silent = true }
-  local keymap = vim.api.nvim_buf_set_keymap
-
-  keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-end
 
 function Spec.common_capabilities()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -34,32 +18,15 @@ function Spec.common_capabilities()
   return capabilities
 end
 
-Spec.on_attach = function(client, bufnr)
-  lsp_keymaps(client, bufnr)
-
-  -- if client.supports_method "textDocument/inlayHint" then
-  --   vim.lsp.inlay_hint.enable(bufnr)
-  -- end
-end
-
--- Spec.toggle_inlay_hints = function ()
---   local bufnr = vim.api.nvim_get_current_buf()
---   vim.lsp.inlay_hint.enable(bufnr, not vim.lsp.inlay_hint.is_enabled(bufnr))
--- end
-
-function Spec.config()
-  local lspconfig = require "lspconfig"
-  local icons = require "diegognt.icons"
-  local servers = require "diegognt.lsp.servers"
-  local whichkey = require "which-key"
-  local default_diagnostic_config = {
+Spec.diagnostics_setup = function()
+  local diagnos1ics_opts = {
     signs = {
       active = true,
       values = {
-        { name = "DiagnosticSignError", text = icons.diagnostics.Error },
-        { name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-        { name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-        { name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
+        { name = "DiagnosticSignError", text = icons.diagnostics.BoldError },
+        { name = "DiagnosticSignWarn", text = icons.diagnostics.BoldWarning},
+        { name = "DiagnosticSignHint", text = icons.diagnostics.BoldHint },
+        { name = "DiagnosticSignInfo", text = icons.diagnostics.BoldInformation },
       },
     },
     virtual_text = false,
@@ -76,56 +43,34 @@ function Spec.config()
     },
   }
 
-  whichkey.register({
-    ["<leader>li"] = { "<cmd>LspInfo<CR>", "Info" },
-    ["<leader>la"] = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action" },
-    ["<leader>lf"] = {
-      "<cmd>lua vim.lsp.buf.format { async = true }<CR>",
-      "Format",
-    },
-    ["<leader>ll"] = {
-      "<cmd>lua vim.lsp.codelens.run()<CR>",
-      "CodeLens Action",
-    },
-    ["<leader>lk"] = {
-      "<cmd>lua vim.lsp.buf.signature_help()<CR>",
-      "Symbol Signature",
-    },
-    ["<leader>lr"] = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
-    ["<leader>ls"] = {
-      "<cmd>Telescope lsp_document_symbols<CR>",
-      "Document Symbols",
-    },
-    ["<leader>lq"] = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "Quick fix" },
-    -- ["<leader>lh"] = { "<cmd>lua require('diegognt.lsp').toggle_inlay_hints()<CR>", "Hints" },
-  })
+  vim.diagnostic.config(diagnos1ics_opts)
 
-  require("lspconfig.ui.windows").default_options.border = "rounded"
-
-  vim.diagnostic.config(default_diagnostic_config)
-
-  for _, sign in
-    ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {})
-  do
-    vim.fn.sign_define(
-      sign.name,
-      { texthl = sign.name, text = sign.text, numhl = sign.name }
-    )
+  for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
   end
+end
 
-  vim.lsp.handlers["textDocument/hover"] =
-    vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-  vim.lsp.handlers["textDocument/signatureHelp"] =
-    vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+Spec.set_keymaps = function()
+  require("which-key").register {
+    ["<leader>li"] = { "<cmd>LspInfo<CR>", "Info" },
+  }
+end
+
+Spec.setup_handlers = function()
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+end
+
+Spec.setup_servers = function()
+  local lspconfig = require "lspconfig"
+  local servers = require "diegognt.lsp.servers"
 
   for _, server in pairs(servers) do
     local opts = {
-      on_attach = Spec.on_attach,
       capabilities = Spec.common_capabilities(),
     }
 
-    local require_ok, settings =
-      pcall(require, "diegognt.lsp.settings." .. server)
+    local require_ok, settings = pcall(require, "diegognt.lsp.settings." .. server)
 
     if require_ok then
       opts = vim.tbl_deep_extend("force", settings, opts)
@@ -133,6 +78,15 @@ function Spec.config()
 
     lspconfig[server].setup(opts)
   end
+end
+
+function Spec.config()
+  require("lspconfig.ui.windows").default_options.border = "rounded"
+
+  Spec.diagnostics_setup()
+  Spec.setup_handlers()
+  Spec.set_keymaps()
+  Spec.setup_servers()
 end
 
 return Spec
