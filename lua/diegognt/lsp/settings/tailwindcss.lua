497d181427d59900e023f2de96d528e5ed61460b
Diego Navarro <code@diegognt.dev>
2024-01-16 16:57:00 -0500

    Fixes the root pattern for TailwindCSS LSP server

--- lua/diegognt/lsp/settings/tailwindcss.lua
+++ lua/diegognt/lsp/settings/tailwindcss.lua
@@ -1,14 +1,10 @@
-local lspconfig_status, lspconfig = pcall(require, "lspconfig")
-
-if not lspconfig_status then
-  vim.notify "The `nvim/lspconfig` plugin was not found."
-  return
-end
+local lspconfig = require "lspconfig"
 
 return {
-  root_dir = lspconfig.util.root_pattern({
+  root_dir = lspconfig.util.root_pattern {
     "tailwind.config.js",
-    "twind.config.ts",
     "tailwind.config.cjs",
-  }),
+    "tailwind.config.mjs",
+    "tailwind.config.ts",
+  }
 }
