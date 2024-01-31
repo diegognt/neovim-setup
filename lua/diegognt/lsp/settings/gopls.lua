857bb83d5b1e639a8002b1075b3affcd6379ebcb
Diego Navarro <code@diegognt.dev>
2024-01-31 16:48:06 -0500

    Enables Inlay hints for gopls

---
+++ lua/diegognt/lsp/settings/gopls.lua
@@ -0,0 +1,17 @@
+local lspconfig = require "lspconfig"
+
+return {
+  settings = {
+    gopls = {
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+    },
+  },
+}
