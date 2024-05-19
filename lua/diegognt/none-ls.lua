0ccf9d2091e2b3f84a8919713189edf34827e2a4
Diego Navarro <code@diegognt.dev>
2024-05-19 17:37:45 -0500

    updates Specs to improve Go LSP

--- lua/diegognt/none-ls.lua
+++ lua/diegognt/none-ls.lua
@@ -1,5 +1,6 @@
 -- NOTE: When using a node based tools used the `dynamic_command`.
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local Spec = {
   "jay-babu/mason-null-ls.nvim",
   event = { "BufReadPre", "BufNewFile" },
@@ -36,6 +37,21 @@ function Spec.config(_, opts)
       formatting.clang_format,
       diagnostics.mypy,
     },
+    on_attach = function(client, bufnr)
+      if client.supports_method "textDocument/formatting" then
+        vim.api.nvim_clear_autocmds {
+          group = augroup,
+          buffer = bufnr,
+        }
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format { bufnr = bufnr }
+          end,
+        })
+      end
+    end,
   }
 end
 
