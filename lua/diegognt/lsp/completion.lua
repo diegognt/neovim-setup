348e91820d565c9f4aa166f27e563bad11ff789c
Diego Navarro <code@diegognt.dev>
2024-08-07 11:02:20 -0500

    updates LSP with configured features

--- lua/diegognt/lsp/completion.lua
+++ lua/diegognt/lsp/completion.lua
@@ -100,7 +100,7 @@ function Spec.common_mapping()
     }),
     ["<CR>"] = cmp.mapping({
       i = function(fallback)
-        if cmp.visible() and cmp.get_active_entry() then
+        if cmp.visible() then
           cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
         else
           fallback()
