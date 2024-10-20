82cf6d9650eb1c59b0b1b685cffaf65939ee2476
Diego Navarro <code@diegognt.dev>
2024-10-20 10:20:17 -0500

    uses loclist for LSP basic actions

--- lua/diegognt/lsp/init.lua
+++ lua/diegognt/lsp/init.lua
@@ -10,39 +10,6 @@ local Spec = {
   },
 }
 
-local function goto_definition(split_cmd)
-  local util = vim.lsp.util
-  local log = require "vim.lsp.log"
-  local api = vim.api
-
-  -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
-  local handler = function(_, result, ctx)
-    if result == nil or vim.tbl_isempty(result) then
-      local _ = log.info() and log.info(ctx.method, "No location found")
-      return nil
-    end
-
-    if split_cmd then
-      vim.cmd(split_cmd)
-    end
-
-    if vim.tbl_islist(result) then
-      util.jump_to_location(result[1])
-
-      if #result > 1 then
-        -- util.set_qflist(util.locations_to_items(result))
-        vim.fn.setqflist(util.locations_to_items(result))
-        api.nvim_command "copen"
-        api.nvim_command "wincmd p"
-      end
-    else
-      util.jump_to_location(result)
-    end
-  end
-
-  return handler
-end
-
 local common_capabilities = function()
   local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
   if status_ok then
@@ -118,7 +85,10 @@ end
 local common_handlers = {
   ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
   ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-  ["textDocument/definition"] = goto_definition "split",
+  ["textDocument/definition"] = vim.lsp.with(vim.lsp.handlers["textDocument/definition"], { loclist = false }),
+  ["textDocument/references"] = vim.lsp.with(vim.lsp.handlers["textDocument/references"], { loclist = true }),
+  ["textDocument/implementation"] = vim.lsp.with(vim.lsp.handlers["textDocument/implementation"], { loclist = true }),
+  ["textDocument/codeAction"] = vim.lsp.with(vim.lsp.handlers["textDocument/codeAction"], { loclist = true }),
 }
 
 local setup_servers = function()
