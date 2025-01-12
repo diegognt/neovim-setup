679c8f7bde6354c63793c4f20c2283440b854218
Diego Navarro <code@diegognt.dev>
2025-01-12 17:40:24 -0500

    adds initial keymaps set up

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,59 @@
+-- Keymaps
+-- Recomendations:
+--  - Look for what the keymap used to do before overwite it
+
+local globals = require "globals"
+local keymap = globals.keymaps.set
+
+--Remap space as leader key
+vim.keymap.set("", "<Space>", "<Nop>")
+
+-- Modes
+--   normal_mode = 'n',
+--   insert_mode = 'i',
+--   visual_mode = 'v',
+--   visual_block_mode = 'x',
+--   term_mode = 't',
+--   command_mode = 'c',
+
+-- Normal Mode --
+-- Better window navigation
+keymap("n", "<M-h>", "<C-w>h")
+keymap("n", "<M-j>", "<C-w>j")
+keymap("n", "<M-k>", "<C-w>k")
+keymap("n", "<M-l>", "<C-w>l")
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>")
+keymap("n", "<C-Down>", ":resize-2<CR>")
+keymap("n", "<C-Left>", ":vertical resize -2<CR>")
+keymap("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Quit vim
+keymap("n", "<Space>q", ":q!<CR>")
+
+-- Insert Mode --
+
+-- Visual Mode --
+-- Stay in indent mode
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
+
+-- Move text up and down
+keymap("v", "<M-j>", ":m .+1<CR>==")
+keymap("v", "<M-k>", ":m .-2<CR>==")
+keymap("v", "p", '"_dP')
+
+-- Visual Block Mode --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv")
+keymap("x", "K", ":move '<-2<CR>gv-gv")
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv")
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv")
+
+-- Terminal Mode --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h")
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j")
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k")
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l")
--- lua/globals/init.lua
+++ lua/globals/init.lua
@@ -1,4 +1,38 @@
+-- Sets a custom keymap.
+-- @param mode "i"|"n"|"v"|"x"|"t"|"c" A Vim mode
+-- @param keymap string The keymap
+-- @param action string|function An action to execute
+-- @param desc? string A description of the keymap
+local set_keymap = function(mode, keymap, action, desc)
+  desc = desc or nil
+  local opts = {
+    n = { noremap = true, silent = true },
+    t = { silent = true },
+    i = {},
+    v = {},
+    x = {},
+    t = {},
+    c = {},
+  }
+
+  if desc ~= nil then
+    vim.keymap.set(
+      mode,
+      keymap,
+      action,
+      vim.tbl_deep_extend("force", opts[mode], { desc = desc })
+    )
+  else
+   vim.keymap.set(mode, keymap, action, opts[mode])
+  end
+end
+
 local Globals = {
+  keymaps = {
+    set = set_keymap,
+    normal_opts = { noremap = true, silent = true },
+    term_opts = { silent = true }
+  },
   paths = {
     python_bin = "~/.pyenv/versions/py3nvim/bin/python"
   }
