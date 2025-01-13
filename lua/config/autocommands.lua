60cafeaca0755d7af39d4351f1b1700b7016a9f1
Diego Navarro <code@diegognt.dev>
2025-01-13 18:51:11 -0500

    adds initial autocommands

---
+++ lua/config/autocommands.lua
@@ -0,0 +1,57 @@
+local GeneralSettingsGroup = vim.api.nvim_create_augroup("GeneralSettings", { clear = true })
+
+-- Closes some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = GeneralSettingsGroup,
+  pattern = {
+    "help",
+    "man",
+    "qf",
+    "loclist",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimResized", {
+  group = ResizeGroup,
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = GeneralSettingsGroup,
+  pattern = "*",
+  desc = "Highlight text on yank",
+  callback = function()
+    require("vim.hl").on_yank({ higroup = "Search", timeout = 200 })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter", "BufRead", "BufNewFile" }, {
+  group = GeneralSettingsGroup,
+  pattern = "*",
+  callback = function()
+    vim.cmd "set formatoptions-=cro"
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = GeneralSettingsGroup,
+  pattern = "qf",
+  command = "set nobuflisted",
+})
+
+-- Go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = GeneralSettingsGroup,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
