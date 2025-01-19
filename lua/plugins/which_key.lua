03f94bf8d3c1088840478d47fe9a14342114f49d
Diego Navarro <code@diegognt.dev>
2025-01-19 15:32:08 -0500

    adds whichkey plugin for keymap help

---
+++ lua/plugins/which_key.lua
@@ -0,0 +1,43 @@
+local globals = require "globals"
+local icons = globals.icons
+
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    plugins = {
+      marks = true, -- shows a list of your marks on ' and `
+      registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+      spelling = {
+        enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+        suggestions = 20, -- how many suggestions should be shown in the list?
+      },
+      -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+      -- No actual key bindings are created
+      presets = {
+        operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+        motions = true, -- adds help for motions
+        text_objects = true, -- help for text objects triggered after entering an operator
+        windows = true, -- default bindings on <c-w>
+        nav = true, -- misc bindings to work with windows
+        z = true, -- bindings for folds, spelling and others prefixed with z
+        g = true, -- bindings for prefixed with g
+      },
+    },
+    -- add operators that will trigger motion and text object completion
+    -- to enable all native operators, set the preset / operators plugin above
+    -- operators = { gc = "Comments" },
+    icons = {
+      breadcrumb = icons.ui.DoubleChevronRight, -- symbol used in the command line area that shows your active key combo
+      separator = icons.ui.BoldArrowRight, -- symbol used between a key and it's label
+      group = icons.ui.Plus, -- symbol prepended to a group
+    },
+    layout = {
+      height = { min = 4, max = 25 }, -- min and max height of the columns
+      width = { min = 20, max = 50 }, -- min and max width of the columns
+      spacing = 3, -- spacing between columns
+      align = "left", -- align columns left, center or right
+    },
+    show_help = true, -- show help message on the command line when the popup is visible
+  },
+}
